c385a010de7974f581b04ba502f7da620b2ee555
CA Lewis <canlewi@sandia.gov>
2019-05-28 18:11:04 -0700

    Final pieces of initial lifting work

---
+++ tests/clang_src2src/pragma_sst_lift.cc
@@ -0,0 +1,28 @@
+
+double fxn(double const* __restrict__ a, int N) {
+  double b = 0;
+
+#pragma sst lift
+  for(auto i = 0; i < N; ++i){
+    b += a[i];
+  }
+
+  return b;
+}
+
+char sstmac_appname_str[] = "pragma_test";
+int main() { 
+  int N = 10;
+  double *a = new double[N]; 
+  
+  const double my_pi = 3.14159265359;
+  const double circ_in_yards = 43825760;
+  for(auto i = 0; i < N; ++i){
+    a[i] = (i * circ_in_yards) / my_pi;
+  }
+   
+  // Be mean to the optimizer
+  a[0] = fxn(a, N);
+
+  return 0; 
+}
