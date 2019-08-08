80c649ee08e8d4ebc26ceb5f54a35fdd233723ce
CA Lewis <canlewi@sandia.gov>
2019-08-08 11:36:52 -0700

    Improve the printing at the end of our program.

--- sstmac/llvm/meminst.cc
+++ sstmac/llvm/meminst.cc
@@ -67,7 +67,64 @@ void sstmac_print_address_info() {
       std::accumulate(LoadAddrs.begin(), LoadAddrs.end(), 0,
                       [](int &val, AddrData const &a) { return val + a.size; });
 
-  std::cout << "Number of stored addresses: " << stored << "\n";
-  std::cout << "Number of loaded addresses: " << loaded << "\n";
+  std::cout << "Number of stored bytes: " << stored << "\n";
+  std::cout << "Number of loaded bytes: " << loaded << "\n\n";
+
+  std::stable_sort(LoadAddrs.begin(), LoadAddrs.end(),
+                   [](AddrData const &a, AddrData const &b) {
+                     return a.thread_id < b.thread_id;
+                   });
+
+  auto max_thread = LoadAddrs.back().thread_id;
+  std::vector<std::pair<AddrData *, AddrData *>> thread_info(max_thread + 1);
+
+  auto find_partitions = [&](int64_t thread_id) {
+    auto Func = [=](AddrData const &a) { return a.thread_id == thread_id; };
+    auto Func2 = [=](AddrData const &a) { return a.thread_id == thread_id + 1; };
+    auto FirstPtr = &*std::find_if(LoadAddrs.begin(), LoadAddrs.end(), Func);
+    auto LastPtr = &*std::find_if(LoadAddrs.begin(), LoadAddrs.end(), Func2);
+
+    return std::make_pair(FirstPtr, LastPtr);
+  };
+
+  for (auto i = 0; i < max_thread + 1; ++i) {
+    thread_info[i] = find_partitions(i);
+  }
+
+  auto test_condition = [&](decltype(thread_info) const &ti) {
+    for (auto const &pair : ti) {
+      if (pair.first != pair.second) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  std::cout << "  | ";
+  for (auto i = 0ul; i < thread_info.size(); ++i) {
+    std::cout << "Thread           " << i << "   ";
+  }
+  std::cout << "\n----";
+  for (auto i = 0ul; i < thread_info.size(); ++i) {
+    std::cout << "---------------------";
+  }
+  std::cout << "\n";
+  auto line = 0;
+  while (test_condition(thread_info)) {
+    if (line >= 10) {
+      std::cout << line++ << "| ";
+    } else {
+      std::cout << " " << line++ << "| ";
+    }
+    for (auto &pair : thread_info) {
+      if (pair.first != pair.second) {
+        std::cout << pair.first->addr << " (" << pair.first->size << ")   ";
+        pair.first += 1;
+      } else {
+        std::cout << "Null                 ";
+      }
+    }
+    std::cout << "\n";
+  }
 }
 }
